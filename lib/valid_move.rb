034ebdc1acb97dd2c7287de75b12cccd82a2a73e
Peter Coughlin <coughlin1966@gmail.com>
2018-02-09 23:42:55 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,3 @@
 def valid_move?(board, index)
-  index.to_i.between?(0, 8) && !board[index] == " " || board[index] == "" ||
-  board[index] == nil
+  index.to_i.between?(0, 8) && !board[index] == " " || board[index] == "" || board[index] == nil
 end
